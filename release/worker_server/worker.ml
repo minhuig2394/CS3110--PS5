45f16e30d0493ba5bf51433f8533763db75d0e3f
minhuig2394 <minhuig2394@gmail.com>
2013-11-13 16:59:26 -0500

    worker.ml
    Added implemented version to new release

--- release/worker_server/worker.ml
+++ release/worker_server/worker.ml
@@ -1,5 +1,9 @@
 open Protocol
 
+let maptable = Hashtbl.create 200
+let redtable = Hashtbl.create 200
+let safe = Mutex.create ()
+
 let send_response client response =
   let success = Connection.output client response in
     (if not success then
@@ -14,15 +18,78 @@ let rec handle_request client =
       begin
         match v with
         | InitMapper source -> 
-          failwith "It's been a long time, old one."
+      print_endline "Intialize Mapper";
+      (let construct = Program.build source in
+      match construct with 
+      |(None, error) ->
+    if (send_response client (Mapper(None, error))
+    then ()
+    else handle_request client
+      |(Some id, "") ->
+    Mutex.lock safe;
+    Hashtbl.add maptable (Some id) true;
+    Mutex.unlock safe;
+    if send_response client (Mapper(Some id, ""))
+    then ()
+    else handle_request client
+      |_ -> failwith "InitMapper: Invalid Compiliation"
         | InitReducer source -> 
-          failwith "Young master, I cannot aid one who opposes the Master!"
+            print_endline "Intialize Reducer";
+      (let construct = Program.build source in
+      match construct with 
+      |(None, error) ->
+    if (send_response client (Reducer(None, error))
+    then ()
+    else handle_request client
+      |(Some id, "") ->
+    Mutex.lock safe;
+    Hashtbl.add redtable (Some id) true;
+    Mutex.unlock safe;
+    if send_response client (Reducer(Some id, ""))
+    then ()
+    else handle_request client
+      |_ -> failwith "InitReducer: Invalid Compiliation"
         | MapRequest (id, k, v) -> 
-          failwith "You won't go unrewarded."
+          print_end_line "Request Mapper";
+      Mutex.lock safe;
+      let found = Hashtbl.mem maptable (Some id) in 
+      Mutex.unlock safe;
+      (match found with 
+      | true -> let result = Program.run id (k,v) in
+        (match result with 
+        | None ->
+      if (send_response client (RuntimeError(id, "MapRequest: None")))
+      then ()
+      else handle_response client
+        | Some n -> 
+      if (send_response client (MapResults(id, n)))
+      then ()
+      else handle_response client)
+      | false -> 
+    if (send_response client (InvalidWorker(id))) 
+    then ()
+    else handle_request client)
         | ReduceRequest (id, k, v) -> 
-          failwith "Really? In that case, just tell me what you need."
+      print_end_line "Request Reducer";
+            Mutex.lock safe;
+      let found = Hashtbl.mem redtable (Some id) in
+      Mutex.unlock safe;
+      (match found with
+      | true -> let result = Program.run id (k, v) in
+        (match result with
+        | None -> 
+      if (send_response client (RuntimeError(id, "ReduceRequest: None"))) 
+      then ()
+      else handle_request client
+        | Some n -> 
+      if (send_response client (ReduceResults(id, n))) then ()
+      else handle_request client)
+      | false -> 
+    if (send_response client (InvalidWorker(id))) then ()
+    else handle_request client)
       end
   | None ->
       Connection.close client;
       print_endline "Connection lost while waiting for request."
+
 
