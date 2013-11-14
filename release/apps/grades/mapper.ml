beea709932668d3ee68d7ddb58d0e37bb5fe0965
dw443 <dw443@users.noreply.github.com>
2013-11-14 18:37:23 -0500

    Update mapper.ml

--- release/apps/grades/mapper.ml
+++ release/apps/grades/mapper.ml
@@ -1,1 +1,10 @@
 open Util;;
+
+let (key, value) = Program.get_input() in
+let split = split_spaces value in
+Program.set_output (List.fold_left (fun acc x ->
+  let tup = 
+    match x with 
+    |h1::h2::[] -> (h1, h2)
+    |_ -> failwith "not possible" in
+  tup::acc) [] split)
