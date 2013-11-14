f5b97576d503acc0ce1ca6bf41dc1206a58a65fa
dw443 <dw443@users.noreply.github.com>
2013-11-14 18:38:19 -0500

    Update grades.ml

--- release/apps/grades/grades.ml
+++ release/apps/grades/grades.ml
@@ -4,9 +4,14 @@ let main (args : string array) : unit =
   if Array.length args < 3 then
     print_endline "Usage: grades <filename>"
   else
-   (*TODO: Write Me! *)
-   failwith "You're in a merry mood today, Mr. Todd."
- 
+   let filename = args.(2) in
+    let students = load_grades filename in 
+    let courses = 
+      List.fold_left (fun acc x ->
+	(split_to_class_lst x.course_grades)::acc) [] students in
+    let reduced = 
+      Map_reduce.map_reduce "grades" "mapper" "reducer" courses in
+    print_reduced_courses reduced
 
 in
 
