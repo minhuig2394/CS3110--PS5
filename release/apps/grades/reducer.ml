fed3fcca91c7c4086017a1afed9f6389067f4ff7
dw443 <dw443@users.noreply.github.com>
2013-11-14 18:02:24 -0500

    Update reducer.ml

--- release/apps/grades/reducer.ml
+++ release/apps/grades/reducer.ml
@@ -1,18 +1,24 @@
 open Util;;
 
 let (key, values) = Program.get_input() in 
-let median = List.fold_left (fun acc x -> 
-  let length = List.length values in 
+let com v1 v2 = 
+  if float_of_string v1 > int_of_string v2 then 1
+  else if float_of_string v1 < float_of_string v2 then -1
+  else 0 in 
+let sortedval = 
+  List.sort com values in
+let median =
+  let length = List.length sortedval in 
   if (length mod 2) = 0 then 
     let mid1 = length - (length/2)in
     let mid2 = mid1 + 1 in
     let mid1int = 
-      int_of_string (List.nth values mid1) in
+      float_of_string (List.nth sortedval mid1) in
     let mid2int = 
-      int_of_string (List.nth values mid2) in
-    let med = (mid1int + mid2int)/2 in 
-    string_of_int med
+      float_of_string (List.nth sortedval mid2) in
+    let med = (mid1int +. mid2int) /. 2. in 
+    string_of_float med
   else 
     let middle = length - ((length - 1)/2) in
-    List.nth values middle) "" values in
+    List.nth sortedval middle in
 Program.set_output [median]
