8a72391bd781e3726c64ab7332cf91d410a9eead
minhuig2394 <minhuig2394@gmail.com>
2013-11-13 23:15:48 -0500

    Works with Reuters now
    I think.

--- release/controller/map_reduce.ml
+++ release/controller/map_reduce.ml
@@ -12,8 +12,8 @@ let hashlock = Mutex.create()
 let tasklock = Mutex.create()
 
 (*Thread Pools*)
-let mthread_pool = Thread_pool.create 100
-let rthread_pool = Thread_pool.create 100
+let mthread_pool = Thread_pool.create 20
+let rthread_pool = Thread_pool.create 20
 
 (* map kv_pairs map_filename initalizes mappers and iterates over
 * kv_pairs, passing work to be attempted by mappers in threads. If the 
@@ -28,6 +28,8 @@ let map kv_pairs map_filename : (string * string) list =
   print_endline "map2";
   let workers = (initialize_mappers map_filename) in 
     (while Hashtbl.length mtasktbl > 0 do 
+      print_endline "task length is ";
+      print_int (Hashtbl.length mtasktbl);
         print_endline "whileloop";
       Hashtbl.iter (fun k v -> 
       print_endline "work";
@@ -45,10 +47,10 @@ let map kv_pairs map_filename : (string * string) list =
                   (Mutex.lock hashlock); 
                   (List.iter (
                     fun elem -> Hashtbl.add mhashtbl (fst elem) (snd elem)) l); 
-                  (Mutex.unlock hashlock);
-                  push_worker workers worker;)
+                  (Mutex.unlock hashlock)
+                  )
               else (); 
-              (Mutex.unlock tasklock)
+              (Mutex.unlock tasklock);push_worker workers worker
             |None -> ()
             ) mthread_pool))
       ) mtasktbl; 
@@ -66,7 +68,8 @@ let map kv_pairs map_filename : (string * string) list =
 * A list is returned where each tuple consists of distinct keys and 
 *their associated values.  
 *)
-let combine kv_pairs : (string * string list) list = 
+let combine kv_pairs : (string * string list) list =
+  print_endline "combining"; 
   List.iter (fun elem -> 
     match elem with 
     |key,value -> Hashtbl.add combinehash key value) kv_pairs;
@@ -83,6 +86,7 @@ let combine kv_pairs : (string * string list) list =
 *)
 
 let reduce kvs_pairs reduce_filename : (string * string list) list =
+  print_endline "reducing";
   List.iter
     (fun elem -> 
       Hashtbl.add rtasktbl elem false) kvs_pairs;
@@ -100,16 +104,17 @@ let reduce kvs_pairs reduce_filename : (string * string list) list =
                 ((Hashtbl.remove rtasktbl k);
                   (Mutex.lock hashlock); 
                     Hashtbl.add rhashtbl key l; 
-                  (Mutex.unlock hashlock);
-                  push_worker workers worker;)
+                  (Mutex.unlock hashlock)
+                  )
               else (); 
-              (Mutex.unlock tasklock)
+              (Mutex.unlock tasklock);push_worker workers worker
             |None -> ()
             ) rthread_pool)) rtasktbl; 
       Thread.delay 0.1;
     done;
   clean_up_workers workers; 
   Thread_pool.destroy rthread_pool; 
+  print_endline "end reducing";
   Hashtbl.fold (fun k v acc -> (k,v)::acc) rhashtbl []
 
 let map_reduce app_name mapper_name reducer_name kv_pairs =
