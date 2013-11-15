82195932c741199b70d308b94ccdbd49b2f7936f
dw443 <dw443@users.noreply.github.com>
2013-11-14 21:30:47 -0500

    Update mapper.ml

--- release/apps/grades/mapper.ml
+++ release/apps/grades/mapper.ml
@@ -2,8 +2,7 @@ open Util;;
 
 let (key, value) = Program.get_input() in
 let courses = 
-  List.fold_left (fun acc x ->
-	(split_to_class_lst x.course_grades)::acc) [] value in
+  split_to_class_lst value in
 let split = List.fold_left (fun acc x -> (split_spaces x)::acc) [] courses in
 Program.set_output (List.fold_left (fun acc x ->
   let tup = 
