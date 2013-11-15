cefe8f6ec7639b3664d377d0c5cc7ab8dc9b8503
dw443 <dw443@users.noreply.github.com>
2013-11-14 22:21:21 -0500

    Update reducer.ml

--- release/apps/grades/reducer.ml
+++ release/apps/grades/reducer.ml
@@ -10,7 +10,7 @@ let sortedval =
 let median =
   let length = List.length sortedval in 
   if (length mod 2) = 0 then 
-    let mid1 = length - (length/2) - 1in
+    let mid1 = length - (length/2) - 1 in
     let mid2 = mid1 + 1 in
     let mid1int = 
       float_of_string (List.nth sortedval mid1) in
